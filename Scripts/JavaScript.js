3e335996428127f4f539c166e5cf64e56f1fb6d4
Data Detective <datadetectiveagent007@gmail.com>
2017-01-01 00:52:59 -0800

    Added Weatehr Widget and cleaned up code

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,53 +1,40 @@
-﻿var set = [];
-var currWeath = [];
-var ret = "";
+﻿var currWeath = [];
 
-function getAPI(position) {
+window.myWidgetParam = { // Parameters for Weather Widget
+    id: 31,
+    cityid: 6173331,
+    appid: '5d350991d758cab06246c66f77821968',
+    containerid: 'openweathermap-widget',
+};
+(function () { // Web call for Weather Widget script
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = 'http://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(script, s);
+})();
+
+if (navigator.geolocation) { // Check for location capability and get weather if true
+    navigator.geolocation.getCurrentPosition(getAPI);
+} else {
+    console.log("Geolocatioin is not supported on your browser.");
+    test.innerHTML = "Geolocatioin is not supported on your browser.";
+}
+
+function getAPI(position) { // Use location to populate weather data call
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
     var i = 0;
-    document.getElementById("locate").innerHTML = 'LAT: ' + lat + '<br/>' + 'LONG: ' + long;
+    console.log('LAT: ' + lat + '\n'  + 'LONG: ' + long);
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long +
-        "&APPID=5d350991d758cab06246c66f77821968", function (data) {
+        "&APPID=5d350991d758cab06246c66f77821968", function (data) { // Populate variables from API data return
         str = JSON.stringify(data.weather, null, 3);
-        console.log(str);
         data.weather.forEach(function (list) {  // Create array of the current weather
-            currWeath.push(list.description);
+            currWeath.push(list.main);
         });
-        console.log(currWeath[0]);
-        document.getElementById("weather").innerHTML = "weather ==> " + currWeath[0];
+        console.log(currWeath, null);
+        document.getElementById("weather").innerHTML = "weather ==> " + currWeath;
         return currWeath;
     });
-    //console.log(currWeath[0]);
-    //document.write(currWeath);
-    document.getElementById("wind").innerHTML = "weather ==>" + currWeath[0]; // Why this doesn't work?
 }
-
-
-navigator.geolocation.getCurrentPosition(getAPI);
-
-
-
-//function getWeather(data) {
-//    for (i = 0; i < data.weather.length; i++) {
-//        document.getElementById("weather").innerHTML += (data.weather[i].description + '<br>');
-//        console.log(data.weather[i]);
-//    }
-//}
-//        var weatherNow = getWeather();
-//console.log(weatherNow);
-
-
-//if (navigator.geolocation) {
-//    navigator.geolocation.getCurrentPosition(userPosition);
-//} else {
-//    console.log("Geolocatioin is not supported on your browser.");
-//    test.innerHTML = "Geolocatioin is not supported on your browser.";
-//}
-
-// **** A string to view API object values -- Call it inside getJSON function ****
-//Object.getOwnPropertyNames(data.contents.quotes[0]).forEach(function (val, idx, array)
-//{
-//    ret += (val + ' -> ' + data.contents.quotes[0][val] + '<br>');
-//    return ret;
-//});
