d75d405a984a929e9ac2728adb95b8c54efbbf95
Data Detective <datadetectiveagent007@gmail.com>
2017-01-03 13:17:24 -0800

    Played with Object and Array manipulation

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -15,12 +15,12 @@
 //    s.parentNode.insertBefore(script, s);
 //})();
 
-    if (navigator.geolocation) { // Check for location capability and get weather if true
-        navigator.geolocation.getCurrentPosition(getAPI);
-    } else {
-        console.log("Geolocatioin is not supported on your browser.");
-        test.innerHTML = "Geolocatioin is not supported on your browser.";
-    }
+if (navigator.geolocation) { // Check for location capability and get weather if true
+    navigator.geolocation.getCurrentPosition(getAPI);
+} else {
+    console.log("Geolocatioin is not supported on your browser.");
+    test.innerHTML = "Geolocatioin is not supported on your browser.";
+}
 
 function getAPI(position) { // Use location to populate weather data call
 
@@ -29,15 +29,19 @@ function getAPI(position) { // Use location to populate weather data call
 
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long +
         "&APPID=5d350991d758cab06246c66f77821968", function (data) { // Populate variables from API data return
+            console.log(data);
+            str = JSON.stringify(data.main, null, 3);
+            console.log(str);
             data.weather.forEach(function (list) {  // Create array of the current weather
-                //Can I do ==> retrun data; and use other global functions to declare required variablles?
-           currWeath.push(list.main);
+                currWeath.push(list);
+            });
+            console.log(currWeath[0].description);
+            document.getElementById("weather").innerHTML = "weather ==> " + currWeath[0].description;
+            return currWeath;
         });
-        console.log(currWeath);
-        document.getElementById("weather").innerHTML = "weather ==> " + currWeath;
-        return currWeath;
-    });
 }
 
+
+
 //console.log('LAT: ' + lat + '\n'  + 'LONG: ' + long);
-//str = JSON.stringify(data.weather, null, 3);
+//str = JSON.stringify(data, null, 3);
