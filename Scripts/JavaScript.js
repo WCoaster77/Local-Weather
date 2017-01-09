1ffc5b0766e30259d00573c99a3c8883c64a9d3c
Data Detective <datadetectiveagent007@gmail.com>
2017-01-09 00:29:54 -0800

    All set to format page all data nad math figured.

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,8 +1,4 @@
-﻿var currWeath = [];
-var cityName = [];
-var data = {};
-var str = 'start';
-
+﻿
 function getPosition() { // Check for location services active and get position
     if (navigator.geolocation) { // Check for location capability and get weather if true
         navigator.geolocation.getCurrentPosition(getAPI);
@@ -22,28 +18,32 @@ function getAPI(position) { // Use location from position to populate weather da
 }
     
 function createStr(data) { // Populate variables from API data return
+
     var coord = data.coord,
         weather = [],
-        icon = new Image(),
         tempK = data.main.temp,
         wind = data.wind,
         country = data.sys.country,
         city = data.name,
-        currweather =
         arrTest = [];
 
     data.weather.forEach(function (list) {
-        weather.push(list.main, (list.icon + '.png')); // Creates string for icon image src string
+        weather.push(list.main, 'http://openweathermap.org/img/w/' + list.icon + '.png'); // Creates string for icon image src string
     });
-    icon.src = 'http://openweathermap.org/img/w/' + weather[1];
 
-    arrTest.push(coord, weather, temp, wind, country, city); // Maybe use this later
+    for (var i = 1, w = 0; w < weather.length; i += 2, w += 2) {
+        var icon = new Image();
+        icon.src = weather[i];
+        document.getElementById("weather").innerHTML += weather[w] + ' ';
+        document.getElementById("icon").appendChild(icon).width = '75';
+    }
 
     document.getElementById("city").innerHTML = city + ', ' + country;
-    document.getElementById("temp").innerHTML = (tempK - 273) + ' C ' + (((tempK - 273) * 9 / 5) + 32) + ' F';
-    document.getElementById("weather").innerHTML = weather[0];
-    document.getElementById("test").appendChild(icon); //doc element must be <div> not <p>
+    document.getElementById("temp").innerHTML = Number(Math.round(tempK - 273 + 'e0') + 'e-0') + ' C ' 
+        + Number(Math.round((tempK - 273) * 9 / 5 + 32 + 'e0') + 'e-0') + ' F';
     document.getElementById("wind").innerHTML = 'Wind Speed: ' + wind.speed + ' Direction: ' + wind.deg;
+
+    arrTest.push(coord, weather, tempK, wind, country, city); // Maybe use this later
 }
 
 function prtAlert(msg) {
@@ -51,32 +51,3 @@ function prtAlert(msg) {
 }
 
 getPosition();
-
-
-
-//            data.weather.forEach(function (list) {  // Create array of the current weather
-//                currWeath.push(list);
-//            });
-//            document.getElementById("weather").innerHTML = "weather ==> " + currWeath[0].description;
-//        });
-//}
-
-//console.log('LAT: ' + lat + '\n'  + 'LONG: ' + long);
-//str = JSON.stringify(data, null, 3);
-
-
-// ***** Weather Widget *****
-//window.myWidgetParam = { // Parameters for Weather Widget
-//    id: 31,
-//    cityid: 6173331,
-//    appid: '5d350991d758cab06246c66f77821968',
-//    containerid: 'openweathermap-widget',
-//};
-//(function () { // Web call for Weather Widget script
-//    var script = document.createElement('script');
-//    script.type = 'text/javascript';
-//    script.async = true;
-//    script.src = 'http://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js';
-//    var s = document.getElementsByTagName('script')[0];
-//    s.parentNode.insertBefore(script, s);
-//})();
